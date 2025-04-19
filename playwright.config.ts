1e42a8b2747a9b27e9e51e80ba3281f746af9298
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2025-04-19 02:34:23 +0000

    test: Comment out tests for firefox.

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,14 +40,14 @@ export default defineConfig({
 		},
 
 		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] },
-		},
-
-		{
 			name: 'webkit',
 			use: { ...devices['Desktop Safari'] },
 		},
+
+		// {
+		// 	name: 'firefox',
+		// 	use: { ...devices['Desktop Firefox'] },
+		// },
 
 		/* Test against mobile viewports. */
 		// {
